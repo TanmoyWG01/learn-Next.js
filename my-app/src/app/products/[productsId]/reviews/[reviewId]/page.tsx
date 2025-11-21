664fccd51d96e0392e534ddcba80f8f57fe326b9
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-11-21 12:35:23 +0530

    Refactor review page logic to handle not found cases correctly

--- my-app/src/app/products/[productsId]/reviews/[reviewId]/page.tsx
+++ my-app/src/app/products/[productsId]/reviews/[reviewId]/page.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { notFound, redirect } from "next/navigation";
 
+// function getRandomInt(count: number) {
+//   return Math.floor(Math.random() * count);
+// }
+
 const Page = async ({
   params,
 }: {
   params: { productsId: string; reviewId: string };
 }) => {
+  // const random = getRandomInt(2);
+  // if(random === 1) {
+  //   throw new Error("Error loading review!");
+  // }
+  // console.log("Random number generated:", random);
   const { productsId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
-    // notFound();
-    redirect("/products")
+    notFound();
+    // redirect("/products")
   }
   return (
     <h1>
