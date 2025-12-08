466aa7648f472649a7c98791e0269eed5c212e88
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-08 13:58:40 +0530

    Update F1Page links to use object syntax for href attributes

--- my-app/src/app/f1/page.tsx
+++ my-app/src/app/f1/page.tsx
@@ -4,7 +4,8 @@ export default function F1Page() {
     return (<>
     <h1>F1 Page</h1>;
     <div>
-        <Link href="/f1/f2">Go to F2 Page</Link>
+        <Link href={"/f1/f2"}>Go to F2 Page</Link>
+        <Link href={"/f3"}>Go to F3 Page</Link>
     </div>
     </>);
 }
