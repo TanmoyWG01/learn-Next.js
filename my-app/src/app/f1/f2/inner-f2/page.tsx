25f44fa6397d53048d5b987ff58b6f0d4e5996ce
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-08 15:38:36 +0530

    Add InnerF2Page component with navigation link to F5

---
+++ my-app/src/app/f1/f2/inner-f2/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function InnerF2Page() {
+    return (<>
+    <h1>Inner F2 Page</h1>
+    <div>
+        <Link href="/f5">Go to Deep F5</Link>
+    </div>
+    </>);
+}
