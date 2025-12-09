dde03853d07d75fabd20a8baf9a002b190c6932e
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-09 15:19:56 +0530

    feat: implement GET, PATCH, and DELETE route handlers for comments; update comments GET handler to support query filtering

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-  return new Response("Hello, Next.js Route Handlers API!");
+import { headers } from "next/headers";
+import {type NextResponse } from "next/server";
+
+export async function GET(request: NextResponse) {  
+
+  // const requestHeader = new Headers(request.headers);
+  // console.log(requestHeader.get("Authorization"));
+
+  const headerList = await headers(); 
+  console.log(headerList.get("Authorization"));
+  return new Response("<h1>Hello, Next.js Route Handlers API!</h1>",{
+    headers: { "Content-Type": "text/html" }
+  });
 }
+
