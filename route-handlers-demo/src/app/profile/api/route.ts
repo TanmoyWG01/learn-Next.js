75c03912b70c4358c8f957abf84850b26091404d
TanmoyWG01 <tanmoy.webgen@gmail.com>
2025-12-10 11:23:53 +0530

    feat: enhance GET route handler to manage cookies and log theme information

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import {type NextResponse } from "next/server";
 
 export async function GET(request: NextResponse) {  
@@ -8,8 +8,20 @@ export async function GET(request: NextResponse) {
 
   const headerList = await headers(); 
   console.log(headerList.get("Authorization"));
+
+  const theme =request.cookies.get("theme");
+  console.log("Cookie Theme:", theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set("ResultPage", "20");
+  console.log(cookieStore.get("ResultPage"));
+  
+
   return new Response("<h1>Hello, Next.js Route Handlers API!</h1>",{
-    headers: { "Content-Type": "text/html" }
+    headers: { 
+      "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark"
+     }
   });
 }
 
